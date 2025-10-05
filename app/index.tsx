4eeb6e54a1c8b4ef2fe59256d553e8d626ed0df5
Mouaz <mouaz.alzahabi@gmail.com>
2025-10-05 23:31:43 +0300

    Reflect changes on app Index

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { StatusBar, StyleSheet, useColorScheme } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { PayslipProvider } from '../app/_context/PayslipsContext';
+import RootNavigator from './navigation/RootNavigator';
 
-export default function Index() {
+export default function App() {
+  const colorScheme = useColorScheme();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <PayslipProvider>
+      <GestureHandlerRootView style={styles.container}>
+        <StatusBar barStyle={colorScheme === 'dark' ? 'light-content' : 'dark-content'} />
+          <RootNavigator /> 
+      </GestureHandlerRootView>
+    </PayslipProvider>
   );
 }
+
+const styles = StyleSheet.create({ container: { flex: 1 } });
